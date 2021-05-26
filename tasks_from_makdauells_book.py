7fa959510b43c502a8f2f10960eda0be17c0237f
atomdistortion <atomdistortion@mail.ru>
2021-05-26 15:52:03 +0500

    Update tasks_from_makdauells_book.py

--- tasks_from_makdauells_book.py
+++ tasks_from_makdauells_book.py
@@ -35,3 +35,35 @@ def once_symb_str(line):
 
 print(once_symb_str("Mamanak"))
 print(once_symb_str("Martnds"))
+
+
+# %% Замена пробелов на %20 задание Q1.3 (метод inplace)
+class my_str_class():
+	def __init__(self, value):
+		self.value = value
+		self.ind = 0
+		self.flag = True
+
+	def symb_inplace(self):
+		ind = self.ind
+		self.value = self.value[:ind] +'%20' + self.value[ind+1:]
+		return self
+	
+	def line_inplace(self):
+		self.flag = True
+		while self.flag:
+			self.flag = True
+			for i, s in enumerate(self.value):
+				if s == " ":
+					self.ind = i
+					self.symb_inplace()
+					break
+				if i == len(self.value) - 1:
+					self.flag = False
+					break
+		return self
+#%%
+str_greet = 'Hello to you, young Padavan!'
+g = my_str_class(str_greet)
+g.line_inplace()
+print(g.value)
